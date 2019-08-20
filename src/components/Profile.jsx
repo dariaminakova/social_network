46b9155253ebb92f5a496629384e7952fdc61fd3
daria_2711 <minakovadawa@gmail.com>
2019-08-20 22:24:25 +0300

    first structure

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img className="cover-img" 
+                src="https://www.sleekcover.com/covers/water-drops-on-plant-facebook-cover.jpg" />
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}; 
+
+export default Profile;
